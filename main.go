628f1f3ef1d7b3288f9b04a44dbae286988cf119
Anshal Shukla <shukla.anshal85@gmail.com>
2023-04-25 01:21:27 +0530

    static pod creation

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
 	"math/rand"
+	"os"
 	"time"
 
 	"path/filepath"
@@ -14,6 +16,8 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
+
+const podManifestDir = "/etc/kubernetes/manifests"
 
 func main() {
 	static("nginx")
@@ -32,6 +36,7 @@ func static(revision string) {
 	}
 
 	podName := revision + "-" + randSeq(9) + "-" + randSeq(5)
+	// podName := revision
 	imageName := revision
 
 	namespace := "static"
@@ -43,29 +48,101 @@ func static(revision string) {
 	}, metav1.CreateOptions{})
 
 	startTime := time.Now()
-	pod := &corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      podName,
-			Namespace: namespace,
-		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:  revision,
-					Image: imageName,
-				},
-			},
-		},
+	// pod := &corev1.Pod{
+	// 	ObjectMeta: metav1.ObjectMeta{
+	// 		Name:      podName,
+	// 		Namespace: namespace,
+	// 	},
+	// 	Spec: corev1.PodSpec{
+	// 		Containers: []corev1.Container{
+	// 			{
+	// 				Name:  revision,
+	// 				Image: imageName,
+	// 				Ports: []corev1.ContainerPort{
+	// 					{
+	// 						Name:          "http",
+	// 						ContainerPort: 8080,
+	// 						Protocol:      corev1.ProtocolTCP,
+	// 					},
+	// 				},
+	// 			},
+	// 		},
+	// 	},
+	// }
+
+	// // Serialize the pod to YAML
+	// podYaml, err := serializeObject(pod)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// // Write the pod YAML to a file
+	// err = ioutil.WriteFile("my-static-pod.yaml", []byte(podYaml), 0644)
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// pod, err = clientset.CoreV1().Pods(namespace).Create(context.TODO(), pod, metav1.CreateOptions{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	imageTag := "latest"
+
+	podManifest := fmt.Sprintf(`
+apiVersion: v1
+kind: Pod
+metadata:
+  name: %s
+spec:
+  containers:
+  - name: %s
+    image: %s:%s
+`, podName, imageName, imageName, imageTag)
+
+	podManifestPath := filepath.Join(podManifestDir, podName+".yaml")
+
+	err = ioutil.WriteFile(podManifestPath, []byte(podManifest), 0644)
+	if err != nil {
+		fmt.Printf("Failed to write pod manifest file: %v\n", err)
+		os.Exit(1)
 	}
 
-	pod, err = clientset.CoreV1().Pods(namespace).Create(context.TODO(), pod, metav1.CreateOptions{})
-	if err != nil {
-		panic(err)
-	}
+	fmt.Printf("Created pod manifest file at %s\n", podManifestPath)
+
+	// podName = podName + "-node-0.anshal-155406.ntu-cloud-pg0.utah.cloudlab.us"
+	// pod, err := clientset.CoreV1().Pods("default").Get(context.Background(), podName, metav1.GetOptions{})
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+
+	// Wait until pod is ready
+	podName = podName + "-node-0.anshal-155406.ntu-cloud-pg0.utah.cloudlab.us"
+	waitForPodReady(clientset, podName)
+
 	endTime := time.Now()
 	duration := endTime.Sub(startTime)
 
-	fmt.Printf("Static pod created with name %s in %v\n", pod.GetName(), duration)
+	fmt.Printf("Static pod created with name %s in %v\n", podName, duration)
+
+	// podIP, err := getPodIP(clientset, namespace, podName)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// // Invoke function on pod IP and port
+	// resp, err := http.Get(fmt.Sprintf("http://%s:%d", podIP, 8080))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer resp.Body.Close()
+
+	// body, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// fmt.Printf("Function response%s\n", string(body))
 }
 
 func randSeq(n int) string {
@@ -77,3 +154,37 @@ func randSeq(n int) string {
 	}
 	return string(b)
 }
+
+func waitForPodReady(clientset *kubernetes.Clientset, podName string) {
+	for {
+		pod, err := clientset.CoreV1().Pods("default").Get(context.Background(), podName, metav1.GetOptions{})
+		if err != nil {
+			continue
+		}
+		pod, err = clientset.CoreV1().Pods(pod.Namespace).Get(context.Background(), pod.Name, metav1.GetOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		if pod.Status.Phase == corev1.PodRunning {
+			break
+		}
+
+		time.Sleep(1 * time.Millisecond)
+	}
+}
+
+func getPodIP(clientset *kubernetes.Clientset, namespace string, name string) (string, error) {
+	pod, err := clientset.CoreV1().Pods(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return "", err
+	}
+	return pod.Status.PodIP, nil
+}
+
+// func serializeObject(obj interface{}) (string, error) {
+// 	serializer := json.NewSerializerWithOptions(json.DefaultMetaFactory, nil, nil, json.SerializerOptions{Yaml: true, Pretty: true})
+// 	result := ""
+// 	err := serializer.Encode(obj, &result)
+// 	return result, err
+// }
